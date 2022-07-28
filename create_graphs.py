24da074c8b0a48f8c0a5f47c1c62f753000f51f8
Keene Chin <keene.chin@gmail.com>
2022-07-27 23:05:52 -0400

    derived fields

--- create_graphs.py
+++ create_graphs.py
@@ -1,10 +1,16 @@
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 with open("reward_trajectories.pkl", 'rb') as logfile:
-    trajectories = pickle.load(logfile)
+    reward_trajs = pickle.load(logfile)
 
 with open("training_data_sizes.pkl", 'rb') as logfile:
     training_groups = pickle.load(logfile)
+
+avg_rewards = [np.mean(rewards) for rewards in reward_trajs] 
+episode_lenghts = [len(rewards) for rewards in reward_trajs]
+pass
 
 
