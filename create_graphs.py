136541dafae147329c2c4c5b28c9866c52fb1c18
Keene Chin <keene.chin@gmail.com>
2022-07-27 23:25:32 -0400

    dataframe construction

--- create_graphs.py
+++ create_graphs.py
@@ -10,7 +10,13 @@ with open("training_data_sizes.pkl", 'rb') as logfile:
     training_groups = pickle.load(logfile)
 
 avg_rewards = [np.mean(rewards) for rewards in reward_trajs] 
-episode_lenghts = [len(rewards) for rewards in reward_trajs]
-pass
+episode_lengths = [len(rewards) for rewards in reward_trajs]
 
+log_data = {"Reward Trajectories" : reward_trajs,
+            "Training Set Group" : training_groups,
+            "Mean Episode Reward" : avg_rewards,
+            "Episode Length" : episode_lengths}
+        
+log_df = pd.DataFrame(log_data)
+pass
 
